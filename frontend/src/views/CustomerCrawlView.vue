7dd83801547fb84e7e00933d80a239a48528f4c6
proydq <ydq@pro-shine.cn>
2025-07-15 18:04:58 +0800

    style: refine sidebar menu

---
+++ frontend/src/views/CustomerCrawlView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import QuickCrawlForm from '../components/QuickCrawlForm.vue'
+import CrawlTaskTable from '../components/CrawlTaskTable.vue'
+import preview from '../mock/previewData.json'
+import tasks from '../mock/crawlTasks.json'
+
+const previewData = ref([])
+const taskList = ref([])
+
+onMounted(() => {
+  previewData.value = preview
+  taskList.value = tasks
+})
+</script>
+
+<template>
+  <div>
+    <div class="chart-container">
+      <QuickCrawlForm :preview-data="previewData" />
+    </div>
+    <div class="chart-container">
+      <CrawlTaskTable :items="taskList" />
+    </div>
+  </div>
+</template>
