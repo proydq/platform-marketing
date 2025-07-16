9592f77a0757401bf8924383de7c116985e3843c
proydq <ydq@pro-shine.cn>
2025-07-16 09:57:23 +0800

    Assign full permissions on login

---
+++ frontend/src/components/RoleCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(['role'])
+</script>
+
+<template>
+  <el-card class="role-card">
+    <div class="role-header">
+      <div>
+        <div class="role-name">{{ props.role.name }}</div>
+        <div class="role-description">{{ props.role.description }}</div>
+      </div>
+    </div>
+    <div class="permission-tags">
+      <el-tag v-for="p in props.role.permissions" :key="p" size="small" type="info">{{ p }}</el-tag>
+    </div>
+    <div class="user-avatar-list">
+      <el-avatar v-for="u in props.role.users" :key="u.id" :src="u.avatar" :size="30" />
+    </div>
+  </el-card>
+</template>
