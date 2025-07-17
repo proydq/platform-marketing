9c99597aba8003408eb6db70248987f59035b1e7
proydq <ydq@pro-shine.cn>
2025-07-17 14:45:34 +0800

    Add super admin initializer

--- backend/src/main/java/com/platform/marketing/repository/RoleRepository.java
+++ backend/src/main/java/com/platform/marketing/repository/RoleRepository.java
@@ -3,7 +3,10 @@ package com.platform.marketing.repository;
 import com.platform.marketing.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
+    Optional<Role> findByName(String name);
+    boolean existsByName(String name);
 }
